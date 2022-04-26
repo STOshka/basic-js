130553cc517f94eb05773b9cb8097143d22be9e7
STOshka <34401821+STOshka@users.noreply.github.com>
2022-04-26 19:04:43 +0300

    feat: add count-cats and sum-digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+    let _n = n;
+    while (_n > 9) {
+      _n = _n.toString().split('').reduce((a,b) => parseInt(a)+parseInt(b), 0);
+    }
+    return _n;
 }
 
 module.exports = {

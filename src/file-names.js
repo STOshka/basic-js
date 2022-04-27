9e7d2cd65160786c8b7e9e0d55a8b42f090dcf8c
STOshka <34401821+STOshka@users.noreply.github.com>
2022-04-27 21:37:22 +0300

    feat: add fileNames and whatSeasons

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let _uniqueNames = [];
+  let _namesObject = {};
+  for (let i=0; i<names.length; i++) {
+      let name = names[i].toString();
+      while (_namesObject[name] != undefined) {
+          let count = _namesObject[name];
+          _namesObject[name]++;
+          name = `${name}(${count})`;           
+      }
+      _uniqueNames.push(name);
+      _namesObject[name] = 1;
+  }
+  return _uniqueNames;
 }
 
 module.exports = {
